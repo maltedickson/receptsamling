d9483e5784ae78990e57fbb94bc8280a65634e9a
Malte Dickson <github-reg@noke.se>
2025-01-04 00:06:45 +0100

    Added tailwind expose colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,33 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
+import exposeColors from '@tailwind-plugin/expose-colors';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Work Sans Variable', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				base: colors.neutral,
+				primary: colors.neutral
+			}
+		}
 	},
 
-	plugins: [typography, forms, containerQueries]
+	plugins: [
+		typography,
+		forms,
+		containerQueries,
+		exposeColors({
+			prefix: '--tw',
+			extract: ['base', 'primary'],
+			mode: 'rgb'
+		})
+	]
 } satisfies Config;
