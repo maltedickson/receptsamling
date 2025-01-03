94b9648ecb9a7a3a5fb993d34887b9d61839f4ad
Malte Dickson <github-reg@noke.se>
2025-01-04 00:11:11 +0100

    Added Work Sans font with fontsource

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource-variable/work-sans';
+	import '@fontsource-variable/work-sans/wght-italic.css';
 	let { children } = $props();
 </script>
 
