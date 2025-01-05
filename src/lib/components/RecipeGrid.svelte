129eb432cacbbe104a9c5b0b4a87148e9c728b21
Malte Dickson <github-reg@noke.se>
2025-01-05 11:36:00 +0100

    Added flip animation to recipe grid

--- src/lib/components/RecipeGrid.svelte
+++ src/lib/components/RecipeGrid.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import type { Recipe } from '$lib/recipe';
 	import RecipeCard from './RecipeCard.svelte';
+	import { flip } from 'svelte/animate';
 	let { recipes }: { recipes: Recipe[] } = $props();
 </script>
 
 <div
 	class="grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] items-start justify-items-center gap-4 md:gap-6"
 >
-	{#each recipes as recipe}
-		<RecipeCard {recipe} />
+	{#each recipes as recipe (recipe.slug)}
+		<div animate:flip={{ duration: 150 }}>
+			<RecipeCard {recipe} />
+		</div>
 	{/each}
 </div>
