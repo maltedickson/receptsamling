e7a85e851c2aae10b72fecc0cf593ebcafb32df2
Malte Dickson <github-reg@noke.se>
2025-01-05 16:37:30 +0100

    Changed in transition for input chips

--- src/lib/components/InputChips.svelte
+++ src/lib/components/InputChips.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { flip } from 'svelte/animate';
 	import InputChip from './InputChip.svelte';
-	import { fade, fly } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 	let { state = $bindable() }: { state: string[] } = $props();
 </script>
 
 <div class="flex flex-wrap gap-2">
 	{#each state as text (text)}
-		<div animate:flip={{ duration: 150 }} in:fly={{ duration: 150, x: 100 }}>
+		<div animate:flip={{ duration: 150 }} in:slide={{ duration: 150, axis: 'x' }}>
 			<InputChip onRemove={() => state.splice(state.indexOf(text), 1)}>
 				{text}
 			</InputChip>
